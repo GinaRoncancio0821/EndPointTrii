b508e89ea18c031356ce03d89b37567f3118f33d
Gina Roncancio Quintana <gina.roncancio.q@gmail.com>
2021-11-04 19:58:47 -0500

    First Commit

---
+++ Controllers/EpisodeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EndPointTrii.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EndPointTrii.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EpisodeController : ControllerBase
+    {
+        
+        private readonly ILogger<EpisodeController> _logger;
+        string URL = "https://rickandmortyapi.com/api/episode";
+        
+
+        public EpisodeController(ILogger<EpisodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] int? id, [FromQuery] string name, [FromQuery] string episode)
+        {
+
+            EpisodeResponse episodelist = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(URL))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    episodelist = JsonConvert.DeserializeObject<EpisodeResponse>(apiResponse);
+                }
+            }
+            var filterList = episodelist.results;
+            if (id != null) filterList = filterList.Where(x => x.id == id).ToList();
+            if (!string.IsNullOrEmpty(name)) filterList = filterList.Where(x => x.name.Contains(name)).ToList();
+            if (!string.IsNullOrEmpty(episode)) filterList = filterList.Where(x => x.episode.Contains(episode)).ToList();
+            episodelist.results = filterList;
+            return Ok(episodelist);
+           
+        } 
+
+    }
+}
---
+++ Response/EpisodeResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPointTrii.Response
+{
+    public class EpisodeResponse
+    {
+        public Info info { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public object prev { get; set; }
+    }
+
+    public class Result
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string air_date { get; set; }
+        public string episode { get; set; }
+        public List<string> characters { get; set; }
+        public string url { get; set; }
+        public DateTime created { get; set; }
+    }
+}
